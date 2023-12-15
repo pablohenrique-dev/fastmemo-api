fb53cea3e3cae74214c7ea7af5684cc3c116c4bf
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-12-15 13:51:18 -0300

    feat: add jsdoc and create a function to shift date

--- src/utils/handleDate.ts
+++ src/utils/handleDate.ts
@@ -1,9 +1,31 @@
+/**
+ * The `formatDateToday` function will return the date of the day it was called. The date will correspond to the beginning of the day.
+ * @returns 
+ */
 export function formatDateToday() {
   const dateToday = new Date();
 
   const year = dateToday.getFullYear();
-  const month = String(dateToday.getMonth() + 1);
-  const day = String(dateToday.getDate());
+  const month = dateToday.getMonth() + 1;
+  const day = dateToday.getDate();
 
-  return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+  const beginningOfTheDay = new Date(Date.UTC(year, month - 1, day, 0, 0, 0));
+
+  return beginningOfTheDay;
+}
+
+/**
+ * The `shiftDayOfADate` function receives two arguments: the first is a date, and the second is a number representing the amount of days to shift the date. The second parameter can be either positive or negative. If it is positive, the result will be a date greater than the one passed, but if it is negative, the date will be earlier.
+ * @param {Date|string} date
+ * @param {number} offsetDays
+ * @returns
+ */
+export function shiftDayOfADate(date: Date | string, offsetDays: number) {
+  const rawDateInMilliseconds = new Date(date).getTime();
+
+  const modifiedDate = new Date(
+    rawDateInMilliseconds + 24 * 60 * 60 * 1000 * offsetDays
+  );
+
+  return modifiedDate;
 }
